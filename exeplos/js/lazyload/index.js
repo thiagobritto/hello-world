caf857f1a1bd1ffac4f065a465f23eab256444db
thiagobritto <thiagobritto.developer@gmail.com>
2021-03-30 08:46:42 -0300

    implementado js/lazyload

---
+++ exeplos/js/lazyload/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const img = document.querySelector('img')
+
+    window.onscroll = () => {
+        if (isVisible(img)) {
+            img.src = img.getAttribute('data-src')
+        }
+    }
+
+})
+
+// lazyload
+function isVisible(element) {
+    let boundingTop = element.getBoundingClientRect().top
+    return boundingTop >= 0 && boundingTop <= window.innerHeight;
+}
